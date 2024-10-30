3aa13b863a534af448180438a31c9b0aacb46d7f
MO518-LAB <mo518@kent.ac.uk>
2024-10-30 12:04:34 +0000

    final

--- starterFile.py
+++ starterFile.py
@@ -3,15 +3,39 @@
 import math
 
 def safe_divide(a, b):
-
+    try:
+       return a / b 
+    except ZeroDivisionError
+       return "Cannot be divided by zero"
 
 def process_list(input_list):
+    input_list = []
+
+
+    except(Error1, Error2, Error3)
+
 
 
 def nested_operations(a, b):
+    try:
+        int(a, b)
+        try:
+            return (a/b)**2
+        except Value Error
+        return ""
+    except ZeroDivisionError
+        return "Cannot be divided by zero" 
 
 
 def process_input():
 
 
 main()
+    print(safe_divide(10, 2))
+    print(safe_divide(10, 0))
+
+    print(process_list([1, '2', 3, 'four', 5]))
+
+    print(nested_operations(16, 4))
+    print(nested_operations(10, 0))
+    print(nested_operations('a', 5))
